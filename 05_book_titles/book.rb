c7f6de4067dc11534af244f56a642f73b0faacef
Keith <kdclemaster@gmail.com>
2019-01-18 16:31:12 -0500

    Book titles finished.

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,28 @@
 class Book
 # write your code here
+	def title
+		@title
+	end
+
+	
+	def title=(string)
+		@title = titlize(string)
+	end
+
+	private
+
+	def titlize(w)
+		w = w.split(' ')
+		lower_words = ['the', 'of', 'a', 'and', 'in', 'an']
+
+		w.each do |word|
+			if !(lower_words.include?(word) and w.index(word) != 0)
+				word.capitalize!
+			end
+		end
+		w = w.join(' ')
+		return w
+	end
+
 end
+
